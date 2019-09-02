b693803c9fc157995df509bbf8c173ea391cf534
Elliot Courant <me@elliotcourant.dev>
2019-09-02 07:57:38 -0500

    Starting to build out encoding and decoding.

--- src/proto/startup_message.rs
+++ src/proto/startup_message.rs
@@ -1,3 +1,6 @@
+use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
+use std::io::{Write, Cursor, Read};
+
 pub type ProtocolVersion = u32;
 
 pub struct StartupMessage {
@@ -12,10 +15,28 @@ pub struct Parameter {
 
 impl Parameter {
     pub fn encode(&self) -> Vec<u8> {
-        let mut result = vec![0; 8 + self.key.len() + self.val.len()];
+        let mut result = vec![0; 0];
+        result.write_u32::<BigEndian>(self.key.len() as u32).unwrap();
+        result.write(self.key.as_ref());
+        result.write_u32::<BigEndian>(self.val.len() as u32).unwrap();
+        result.write(self.val.as_ref());
         return result.to_vec();
     }
+
+    fn decode(&mut self, src: Vec<u8>) {
+        let mut rdr = Cursor::new(src);
+        let key_size = rdr.read_u32::<BigEndian>().unwrap();
+        let mut key = vec![0; key_size as usize];
+        let res = rdr.read_exact(key.as_mut());
+        self.key = String::from_utf8(key).unwrap();
+
+        let val_size = rdr.read_u32::<BigEndian>().unwrap();
+        let mut val = vec![0; val_size as usize];
+        let res = rdr.read_exact(val.as_mut());
+        self.val = String::from_utf8(val).unwrap();
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -28,8 +49,11 @@ mod tests {
 
     #[test]
     fn test_parameter_encode() {
-        let param = Parameter { key: String::from("1"), val: String::from("1") };
+        let param = Parameter { key: String::from("user"), val: String::from("postgres") };
         let result = param.encode();
-        println!("test")
+        let mut decoded = Parameter{ key: "".to_string(), val: "".to_string() };
+        decoded.decode(result);
+        assert_eq!(decoded.key, param.key);
+        assert_eq!(decoded.val, param.val);
     }
 }
